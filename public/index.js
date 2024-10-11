d0183ab6a326a5731673da5779bb8f6416a53bbf
Hong Shick Pak <hong@hspak.com>
2024-10-10 22:54:38 -0700

    feat: filtering

--- public/index.js
+++ public/index.js
@@ -1,22 +1,33 @@
 const MAX_ATTR = 20;
 const MAX_LINES = 5000;
-const MAX_DATA = 20_000;
+const MAX_DATA = 100_000;
 
 let last = -1;
 let appState = null;
 let rows = [];
 let data = [];
+let filteredData = [];
 let entrypoint = document.getElementById('stuff');
 
 let sidenav = document.getElementById('attributes');
 let attrRows = [];
+
+// {
+//   attrName: {
+//     attr1: {
+//       count: number,
+//       filter: bool,
+//     }
+//   }
+// }
 let attributes = {
   filename: {}
 };
 
-const fuse = new Fuse();
+let filters = [];
 
-reservedNames = [
+
+const reservedNames = [
   'message',
   'timestamp',
 ];
@@ -26,25 +37,34 @@ function accumUniqueAttrs(structuredLog, filename) {
     if (reservedNames.includes(key)) {
       continue;
     }
-    val = structuredLog[key];
+    const val = structuredLog[key];
     if (!(key in attributes)) {
       attributes[key] = {};
     }
     if (val in attributes[key]) {
-      attributes[key][val] += 1;
+      attributes[key][val]['count'] += 1;
     } else {
-      attributes[key][val] = 1 ;
+      attributes[key][val] = {};
+      attributes[key][val]['count'] = 1;
     }
   }
   if (!(filename in attributes['filename'])) {
-    attributes['filename'][filename] = 1
+    attributes['filename'][filename] = {}
+    attributes['filename'][filename]['count'] = 1
   } else {
-    attributes['filename'][filename] += 1
+    attributes['filename'][filename]['count'] += 1
   }
 }
 
-function filterData() {
+// TODO: incomplete
+function filter() {
+  if (filters.length > 0) {
+    filteredData = data.filter((dat) => {
+      return dat.filename == filters[0]
+    });
+  }
 }
+ 
 
 function populate(msg) {
   msg.forEach((blob) => {
@@ -73,6 +93,7 @@ function populate(msg) {
 
     lines.forEach((line) => {
       if (last === MAX_DATA-1) {
+        // TODO: this is probably slow
         for (let i=0; i < MAX_DATA-1; i++) {
           data[i] = data[i+1];
         }
@@ -82,7 +103,14 @@ function populate(msg) {
       data[last] = line;
     });
   }); 
+
+  filter();
 }
+
+const options = {
+  keys: ['message', 'filename'],
+};
+const fuse = new Fuse(data);
 
 function renderSidenav() {
   Object.entries(attributes).forEach(([attrName, values], i) => {
@@ -95,36 +123,71 @@ function renderSidenav() {
     attrRows[i].append(div);
     Object.entries(values).forEach(([valName, val]) => {
       const div = document.createElement('div');
-      const text = document.createTextNode(`${valName}: ${val}`);
+      if ('meta' in val && val['meta'] == true) {
+        div.setAttribute("data-test", val['meta']);
+        div.classList.add('selected')
+      }
+      const text = document.createTextNode(`${valName}: ${val.count}`);
       div.classList.add('attribute-item')
       div.append(text);
+      div.onclick = () => { 
+        if ('meta' in val) {
+          val['meta'] = !val['meta'];
+        } else {
+          val['meta'] = true;
+        }
+
+        const filterKey = valName;
+        if (val['meta']) {
+          filters.push(filterKey);
+        } else {
+          filters = filters.filter(item => item != filterKey);
+        }
+
+        console.log(filterKey)
+        render();
+      };
       attrRows[i].append(div);
     });
   });
 }
 
 function render() {
-  const offset = last > MAX_LINES ? last - MAX_LINES : 0;
-  const cap = Math.min(MAX_LINES, last);
-  for (let i=0; i<cap; i++) {
-      rows[i].nodeValue = `${data[i+offset].timestamp} ${data[i+offset].filename}: ${data[i+offset].message}`;
+  if (filters.length > 0) {
+    const len = filteredData.length;
+    console.log('filtered', len)
+    const offset = len > MAX_LINES ? len - MAX_LINES : 0;
+    const cap = Math.min(MAX_LINES, len);
+    for (let i=0; i<cap; i++) {
+        rows[i].nodeValue = `${filteredData[i+offset].timestamp} ${filteredData[i+offset].filename}: ${filteredData[i+offset].message}`;
+    } 
+
+    // Clear
+    const fullCap = Math.min(MAX_LINES, last);
+    for (let i=len; i<fullCap; i++) {
+        rows[i].nodeValue = ``;
+    } 
+  } else {
+    console.log('all')
+    const offset = last > MAX_LINES ? last - MAX_LINES : 0;
+    const cap = Math.min(MAX_LINES, last);
+    for (let i=0; i<cap; i++) {
+        rows[i].nodeValue = `${data[i+offset].timestamp} ${data[i+offset].filename}: ${data[i+offset].message}`;
+    }
   }
+  renderSidenav();
 }
 
 for(let i = 0; i < MAX_LINES; i++) {
   const div = document.createElement('div');
   const text = document.createTextNode('');
   div.replaceChildren(text);
-  // div.onclick = () => { alert('blah'); };
   entrypoint.append(div);
   rows.push(text);
 }
 
 for(let i = 0; i < MAX_ATTR; i++) {
   const div = document.createElement('div');
-  div.onclick = () => { 
-    render();
-  };
   sidenav.append(div);
   attrRows.push(div);
 }
@@ -137,7 +200,6 @@ socket.onmessage = (event) => {
     data.push(d)
     populate(d);
     render();
-    renderSidenav();
   } else if (event.data === 'hello') {
     appState = "ready";
   } else {
